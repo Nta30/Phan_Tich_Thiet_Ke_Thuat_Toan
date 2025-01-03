bf37e1fbc27f194cc7eec6faaef6f97c16e62c8c
Nta30 <164646563+Nta30@users.noreply.github.com>
2025-01-03 12:58:48 +0700

    Add files via upload

---
+++ 1.6.Tim_Diem_Trong_Hinh_Tron_Gan_1_Diem_Cho_Truoc.cpp
@@ -0,0 +1,28 @@
+//Tìm điểm trong hình tròn và gần một điểm cho trước nhất
+//Link bài: https://laptrinhonline.club/problem/tichpxdiemgan
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef pair<double,double> Diem;
+#define x first
+#define y second
+ 
+ //Hàm tính bình phương khoảng cách giữa 2 điểm
+ double bpkc(Diem A, Diem B){
+    return pow(A.x-B.x,2) + pow(A.y-B.y,2);
+ }
+
+ int main(){
+   Diem O,A,B,M;
+   double r;
+   cin>>O.x>>O.y>>r;
+   cin>>A.x>>A.y;
+   B=O;
+   while(fabs(A.x-B.x)>1e-4 || fabs(A.y-B.y)>1e-4){
+      M.x = (A.x + B.x)/2;
+      M.y = (A.y + B.y)/2;
+      (bpkc(O,M)>r*r)?(A=M):(B=M);
+   }
+   cout<<setprecision(3)<<fixed<<"("<<A.x<<", "<<A.y<<")";
+ }
